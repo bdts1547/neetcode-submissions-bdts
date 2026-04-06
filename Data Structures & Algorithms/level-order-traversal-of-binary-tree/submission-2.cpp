2d9476e6797195da4a17d759ce820789a47a5e42
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-04-06 19:42:11 +0700

    Add: level-order-traversal-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-2.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    // map<int, vector<int>> m;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        map<int, vector<int>> m;
+        if (!root) return res;
+        res.push_back(vector<int> {root->val});
+        int depth = 1;
+        travel(root, depth, m);
+        for (auto [k, v]: m) {
+            // printf("%d\n", k);
+            // for (auto num: v) {
+                // printf("%d ", num);
+                res.push_back(v);
+            // }
+        }
+        return res;
+    }
+    void travel(TreeNode* node, int depth, map<int, vector<int>>& m) {
+        vector<int> res;
+        if (!node) return ;
+        if (node->left) m[depth].push_back(node->left->val);
+        if (node->right) m[depth].push_back(node->right->val);
+        travel(node->left, depth + 1, m);
+        travel(node->right, depth + 1, m);
+    }
+};
