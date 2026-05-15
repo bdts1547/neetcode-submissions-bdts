2f3d0c0cf6c376b907a1986e6403ce86325a6d95
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-05-15 11:09:28 +0700

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> subset;
+        vector<bool> visited (nums.size(), false);
+        backtracking(0, nums, visited, subset, res);
+        return vector<vector<int>> (res.begin(), res.end());
+    }
+
+    void backtracking(int i, const vector<int>& nums, vector<bool>& visited,
+    vector<int>& subset, vector<vector<int>>& res) {
+        if (subset.size() == nums.size()) {
+            res.push_back(subset);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && visited[i-1] && nums[i] == nums[i-1]) continue;
+            if (!visited[i]) {
+                subset.push_back(nums[i]);
+                visited[i] = true;
+                backtracking(i+1, nums, visited, subset, res);
+                subset.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+};
