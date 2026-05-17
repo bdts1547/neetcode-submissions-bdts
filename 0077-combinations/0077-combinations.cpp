4f44ca3c5ef4bfcea4e24bcc6f87c94fda241744
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-05-17 11:15:34 +0700

    Time: 35 ms (91.81%), Space: 62.6 MB (68.9%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> subset;
+        backtracking(1, n, k, subset, res);
+        return res;
+    }
+
+    void backtracking(int i, int n, int k, vector<int>& subset, vector<vector<int>>& res) {
+        if (k == subset.size()) {
+            res.push_back(subset);
+            return;
+        }
+
+        for (int j = i; j <= n; j++) {
+            subset.push_back(j);
+            backtracking(j+1, n, k, subset, res);
+            subset.pop_back();
+        }
+    }
+};
