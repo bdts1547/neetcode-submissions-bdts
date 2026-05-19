b5481c38875f7115c5b1c37158e408be020bdb69
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-05-19 12:40:59 +0700

    Time: 317 ms (45.84%), Space: 11.8 MB (18.36%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int ROW, COL, res =false;
+    int ROW, COL, res = false;
     bool exist(vector<vector<char>>& board, string word) {
         ROW = board.size();
         COL = board[0].size();
@@ -16,46 +16,37 @@ public:
         return false;
     }
 
-    void backtracking(const vector<vector<char>>& board, string word, int i, int r, int c,
+    void backtracking( vector<vector<char>>& board, string word, int i, int r, int c,
     vector<vector<bool>>& visited) {
         if (i == word.size()) {
             res = true;
             return;
         }
+        char temp = board[r][c];
+        board[r][c] = '?'; // mark visited  
 
         if (i < word.size() && c + 1 < COL && board[r][c+1] == word[i] && !visited[r][c+1]) {
-            // printf("left\n");
-            visited[r][c] = true;
             backtracking(board, word, i+1, r, c+1, visited);
-            visited[r][c] = false;
         }
 
         // right
         if (i < word.size() &&  c - 1 >= 0 && board[r][c-1] == word[i] && !visited[r][c-1]) {
-            // printf("right\n");
-            visited[r][c] = true;
             backtracking(board, word, i+1, r, c-1, visited);
-            visited[r][c] = false;
 
         }
 
         // top
         if (i < word.size() &&  r-1 >= 0 && board[r-1][c] == word[i] && !visited[r-1][c]) {
-            // printf("top\n");
-            visited[r][c] = true;
             backtracking(board, word, i+1, r-1, c, visited);
-            visited[r][c] = false;
 
         }
 
         // down
         if (i < word.size() &&  r+1 < ROW && board[r+1][c] == word[i] && !visited[r+1][c]) {
-            // printf("down\n");
-            visited[r][c] = true;
             backtracking(board, word, i+1, r+1, c, visited);
-            visited[r][c] = false;
-
         }
+
+        board[r][c] = temp;
 
     }
 };
