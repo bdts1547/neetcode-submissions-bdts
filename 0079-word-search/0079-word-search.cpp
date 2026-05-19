079b2571e5a6792eedc417a2724b252ae06e4932
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-05-19 12:44:21 +0700

    Time: 293 ms (48.58%), Space: 11.6 MB (29.08%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,11 +4,10 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         ROW = board.size();
         COL = board[0].size();
-        vector<vector<bool>> visited(ROW, vector<bool> (COL, false));
         for (int r = 0; r < ROW; r++) {
             for (int c = 0; c < COL; c++) {
                 if (board[r][c] == word[0]) {
-                    backtracking(board, word, 1, r, c, visited);
+                    backtracking(board, word, 1, r, c);
                     if (res) return res;
                 }
             }
@@ -16,8 +15,7 @@ public:
         return false;
     }
 
-    void backtracking( vector<vector<char>>& board, string word, int i, int r, int c,
-    vector<vector<bool>>& visited) {
+    void backtracking( vector<vector<char>>& board, string word, int i, int r, int c) {
         if (i == word.size()) {
             res = true;
             return;
@@ -25,25 +23,25 @@ public:
         char temp = board[r][c];
         board[r][c] = '?'; // mark visited  
 
-        if (i < word.size() && c + 1 < COL && board[r][c+1] == word[i] && !visited[r][c+1]) {
-            backtracking(board, word, i+1, r, c+1, visited);
+        if (i < word.size() && c + 1 < COL && board[r][c+1] == word[i]) {
+            backtracking(board, word, i+1, r, c+1);
         }
 
         // right
-        if (i < word.size() &&  c - 1 >= 0 && board[r][c-1] == word[i] && !visited[r][c-1]) {
-            backtracking(board, word, i+1, r, c-1, visited);
+        if (i < word.size() &&  c - 1 >= 0 && board[r][c-1] == word[i]) {
+            backtracking(board, word, i+1, r, c-1);
 
         }
 
         // top
-        if (i < word.size() &&  r-1 >= 0 && board[r-1][c] == word[i] && !visited[r-1][c]) {
-            backtracking(board, word, i+1, r-1, c, visited);
+        if (i < word.size() &&  r-1 >= 0 && board[r-1][c] == word[i]) {
+            backtracking(board, word, i+1, r-1, c);
 
         }
 
         // down
-        if (i < word.size() &&  r+1 < ROW && board[r+1][c] == word[i] && !visited[r+1][c]) {
-            backtracking(board, word, i+1, r+1, c, visited);
+        if (i < word.size() &&  r+1 < ROW && board[r+1][c] == word[i]) {
+            backtracking(board, word, i+1, r+1, c);
         }
 
         board[r][c] = temp;
