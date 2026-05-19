52b3d16daf8f25e3e261d74df23ca2e494435f5d
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-05-19 12:34:49 +0700

    Time: 359 ms (43.39%), Space: 11.7 MB (18.36%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int ROW, COL, res =false;
+    bool exist(vector<vector<char>>& board, string word) {
+        ROW = board.size();
+        COL = board[0].size();
+        vector<vector<bool>> visited(ROW, vector<bool> (COL, false));
+        for (int r = 0; r < ROW; r++) {
+            for (int c = 0; c < COL; c++) {
+                if (board[r][c] == word[0]) {
+                    backtracking(board, word, 1, r, c, visited);
+                    if (res) return res;
+                }
+            }
+        }
+        return false;
+    }
+
+    void backtracking(const vector<vector<char>>& board, string word, int i, int r, int c,
+    vector<vector<bool>>& visited) {
+        if (i == word.size()) {
+            res = true;
+            return;
+        }
+
+        if (i < word.size() && c + 1 < COL && board[r][c+1] == word[i] && !visited[r][c+1]) {
+            // printf("left\n");
+            visited[r][c] = true;
+            backtracking(board, word, i+1, r, c+1, visited);
+            visited[r][c] = false;
+        }
+
+        // right
+        if (i < word.size() &&  c - 1 >= 0 && board[r][c-1] == word[i] && !visited[r][c-1]) {
+            // printf("right\n");
+            visited[r][c] = true;
+            backtracking(board, word, i+1, r, c-1, visited);
+            visited[r][c] = false;
+
+        }
+
+        // top
+        if (i < word.size() &&  r-1 >= 0 && board[r-1][c] == word[i] && !visited[r-1][c]) {
+            // printf("top\n");
+            visited[r][c] = true;
+            backtracking(board, word, i+1, r-1, c, visited);
+            visited[r][c] = false;
+
+        }
+
+        // down
+        if (i < word.size() &&  r+1 < ROW && board[r+1][c] == word[i] && !visited[r+1][c]) {
+            // printf("down\n");
+            visited[r][c] = true;
+            backtracking(board, word, i+1, r+1, c, visited);
+            visited[r][c] = false;
+
+        }
+
+    }
+};
