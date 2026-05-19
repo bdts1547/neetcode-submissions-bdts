292fc189f484a20df8f834e728fbbe08c35550b2
Nguyễn Hải Đăng <66860249+bdts1547@users.noreply.github.com>
2026-05-19 12:47:34 +0700

    Time: 295 ms (48.36%), Space: 11.5 MB (38.54%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-    int ROW, COL, res = false;
+    int ROW, COL;
     bool exist(vector<vector<char>>& board, string word) {
         ROW = board.size();
         COL = board[0].size();
         for (int r = 0; r < ROW; r++) {
             for (int c = 0; c < COL; c++) {
                 if (board[r][c] == word[0]) {
-                    backtracking(board, word, 1, r, c);
-                    if (res) return res;
+                    if(backtracking(board, word, 1, r, c)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
 
-    void backtracking( vector<vector<char>>& board, string word, int i, int r, int c) {
+    bool backtracking(vector<vector<char>>& board, string word, int i, int r, int c) {
         if (i == word.size()) {
-            res = true;
-            return;
+            return true;
         }
-        char temp = board[r][c];
-        board[r][c] = '?'; // mark visited  
 
+        char tmp = board[r][c];
+        board[r][c] = '?'; // visited
+
+        // left
         if (i < word.size() && c + 1 < COL && board[r][c+1] == word[i]) {
-            backtracking(board, word, i+1, r, c+1);
+            if(backtracking(board, word, i+1, r, c+1)) return true;
         }
 
         // right
         if (i < word.size() &&  c - 1 >= 0 && board[r][c-1] == word[i]) {
-            backtracking(board, word, i+1, r, c-1);
+            if(backtracking(board, word, i+1, r, c-1)) return true;
 
         }
 
         // top
         if (i < word.size() &&  r-1 >= 0 && board[r-1][c] == word[i]) {
-            backtracking(board, word, i+1, r-1, c);
+            if(backtracking(board, word, i+1, r-1, c)) return true;
 
         }
 
         // down
         if (i < word.size() &&  r+1 < ROW && board[r+1][c] == word[i]) {
-            backtracking(board, word, i+1, r+1, c);
+            if(backtracking(board, word, i+1, r+1, c)) return true;
+
         }
 
-        board[r][c] = temp;
+        board[r][c] = tmp;
+        return false;
 
     }
 };
